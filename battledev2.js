e37e5d1b3d0df14d906b9abc491bd2dbffaee6af
Dinasoa <99045924+Dinasoa@users.noreply.github.com>
2022-02-20 02:54:02 -0900

    Update battledev2.js

--- battledev2.js
+++ battledev2.js
@@ -2,11 +2,15 @@ const prompt =  require("prompt-sync")();
 
 let numberOfButton = +prompt("Enter the total number of button: ");
 
+// Function that searches the correct button among the buttons present in the cockpit 
+
 const findButton = () => {
     let obj = {}
     for (let i = 0; i < numberOfButton; i++) {
         let buttonName = prompt("Enter the name of all cockpits: ")
       
+        //Count the occurence of all the buttons 
+        
         if(obj[buttonName]){
             obj[buttonName]++
         }
@@ -15,8 +19,10 @@ const findButton = () => {
     }
 
     let newArr = Object.entries(obj)
-   
     let button = []
+    
+    //Here , we will find which of the buttons is repeated twice 
+    
     for (let i = 0; i < newArr.length; i++) {
         for (let j = 0; j < newArr[i].length; j++) {
             if(newArr[i][1] == 2){
