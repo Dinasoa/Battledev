bf8326a76f8c2904f71573da7ea2424c0ef72e74
Dinasoa <99045924+Dinasoa@users.noreply.github.com>
2022-02-20 01:31:16 -0900

    Update battledev2.js

--- battledev2.js
+++ battledev2.js
@@ -1,24 +1,28 @@
-const prompt = require("prompt-sync")();
-let nbrebouton = +prompt("Saisir le nombre total des cockpits: ")
+const prompt =  require("prompt-sync")();
 
-let obj = {}
-for(let i = 0 ; i<nbrebouton ; i++){
-    let nomBouton = prompt ("Saisir le nom de chaque cockpit: ")
+let numberOfButton = +prompt("Enter the total number of button: ");
 
-    if(obj[nomBouton])
-    obj[nomBouton]++
-    else obj[nomBouton] = 1
+const findButton = () => {
+    let obj = {}
+    for (let i = 0; i < numberOfButton; i++) {
+        let buttonName = prompt("Enter the name of all cockpits: ")
+      
+        if(obj[buttonName]){
+            obj[buttonName]++
+        }
+        else obj[buttonName] = 1
+        
+    }
 
-}
-
-let newTab = Object.entries(obj)
-console.log(newTab);
-
-for (let i = 0; i < newTab.length; i++) {
-    for (let j = 0; j < newTab[i].length; j++) {
-        console.log(newTab[i]);
-        if(newTab[i][1]==2){
-            console.log(newTab[i][0])
+    let newArr = Object.entries(obj)
+   
+    let button = []
+    for (let i = 0; i < newArr.length; i++) {
+        for (let j = 0; j < newArr[i].length; j++) {
+            if(newArr[i][1] == 2){
+                button.push(newArr[i][0])
+            }
         }
     }
+    return button[0]
 }
